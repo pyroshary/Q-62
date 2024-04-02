000549f0200db6fd0184a22cb5464677ca0ad710
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 00:24:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+;
+var Student = {
+    name: "Shary",
+    age: 23,
+    Languages: ["HTML", "CSS", "JAVA", "TYPE SCRIPT"]
+};
+console.log(Student);
---
+++ index.ts
@@ -0,0 +1,16 @@
+//Making student interface
+interface Student{
+    name: string;
+    age:number;
+    Languages:string[]
+};
+//adding data in interface
+let Student: Student ={
+    name: "Shary",
+    age: 23,
+    Languages:["HTML","CSS","JAVA","TYPE SCRIPT"]
+}
+
+//prinitng data filled in interface
+
+console.log(Student);
